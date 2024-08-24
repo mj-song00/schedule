2d6b3f4bd4753ba32d6a21835bcc1159b6e918bc
mj-song00 <niveus128@gmail.com>
2024-08-25 06:16:59 +0900

    Chore: modify schedule.sql

--- schedule.sql
+++ schedule.sql
@@ -1,16 +1,16 @@
+drop table if exists post;
 create Table `post` (
                         userId int not null primary key auto_increment,
                         contents varchar(100) not null,
                         password varchar(20) not null,
-                        manager varchar(10) not null,
                         createdAt date not null,
                         updatedAt date
 );
 
-
+drop table if exists comment;
 create Table `comment`(
                           commentId int not null primary key auto_increment,
-                          commetContents varchar(100) not null
+                          comments varchar(100) not null
                           createdAt date not null,
                           updatedAt date
 );
